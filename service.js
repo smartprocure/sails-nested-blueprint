e7c59cc4b856788c089f3a4b76924b19ea096237
Dimitar Shishkov <dshishkov@smartprocure.us>
2017-08-29 16:49:59 -0400

    Test create and destroy with cascade and soft delete

--- service.js
+++ service.js
@@ -1,60 +1,7 @@
 let _ = require('lodash/fp')
 
-module.exports = (models, modelName) => ({
-  createNested: async record => {
-    let model = models[modelName]
-
-    let associationIds = _.flow(
-      _.map('alias'),
-      _.filter(id => _.isPlainObject(record[id]))
-    )(model.associations)
-
-    let {id} = await model.create(_.omit(associationIds, record)).meta({fetch: true}).then()
-
-    let updates = await Promise.all(_.map(async association => {
-      // Get Child Info
-      let childRecord = record[association.alias]
-      if (!childRecord || _.isString(childRecord)) return {}
-
-      let childModel = models[association[association.type]]
-      let childModelAssociation = _.find({collection: modelName}, childModel.associations) ||
-                                  _.find({model: modelName}, childModel.associations)
-
-      // Create child
-      childRecord[childModelAssociation.alias] = childModelAssociation.type === 'collection' ? [id] : id
-      let {childId} = await childModel.create(childRecord).meta({fetch: true}).then()
-
-      return {
-        [association.alias]: association.type === 'collection' ? [childId] : childId
-      }
-    }, model.associations))
-
-    await model.update({id})
-      .set(_.reduce(_.extend, {}, updates))
-      .then()
-
-    return _.extend({statusCode: 201}, await model.findOne({id}).then())
-  },
-  // destroyNested: async record => {
-  //   let model = models[modelName]
-  //   let id = record.id
-  //
-  //   await Promise.all(_.map(async association => {
-  //     // Get Child Info
-  //     let childRecord = record[association.alias]
-  //     if (!childRecord) return
-  //
-  //     let childModel = models[association[association.type]]
-  //
-  //     // Destroy child
-  //     await childModel.destroy(childRecord).then()
-  //   }, model.associations))
-  //
-  //   await model.destroy({id})
-  //
-  //   return 200
-  // },
-  destroy: async (options, record) => {
+module.exports = (models, modelName) => {
+  let destroy = _.curry(async (options, record) => {
     let {soft = false, cascade = false, customDelete, beforeDelete} = options
     let model = models[modelName]
     let id = record.id
@@ -82,5 +29,44 @@ module.exports = (models, modelName) => ({
     }
 
     return 200
+  })
+
+  return {
+    createNested: async record => {
+      let model = models[modelName]
+
+      let associationIds = _.flow(
+        _.map('alias'),
+        _.filter(id => _.isPlainObject(record[id]))
+      )(model.associations)
+
+      let {id} = await model.create(_.omit(associationIds, record)).meta({fetch: true}).then()
+
+      let updates = await Promise.all(_.map(async association => {
+        // Get Child Info
+        let childRecord = record[association.alias]
+        if (!childRecord || _.isString(childRecord)) return {}
+
+        let childModel = models[association[association.type]]
+        let childModelAssociation = _.find({collection: modelName}, childModel.associations) ||
+          _.find({model: modelName}, childModel.associations)
+
+        // Create child
+        childRecord[childModelAssociation.alias] = childModelAssociation.type === 'collection' ? [id] : id
+        let {childId} = await childModel.create(childRecord).meta({fetch: true}).then()
+
+        return {
+          [association.alias]: association.type === 'collection' ? [childId] : childId
+        }
+      }, model.associations))
+
+      await model.update({id})
+        .set(_.reduce(_.extend, {}, updates))
+        .then()
+
+      return _.extend({statusCode: 201}, await model.findOne({id}).then())
+    },
+    destroy,
+    destroyNested: destroy({cascade: true})
   }
-})
+}
